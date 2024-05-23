773537c3951c5b35daee7033e9f0436cc53ff5cb
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2024-05-23 12:28:52 +0500

    007 task added

---
+++ codeWarsTasks/007. 7kyu.Shortest_word.swift
@@ -0,0 +1,20 @@
+import Foundation
+func shortestWord(){
+    let array = "bitcoin take over the world maybe who knows perhaps"
+    print(find_short(array))
+}
+
+func find_short(_ str: String) -> Int{
+    let words = str.components(separatedBy: CharacterSet.whitespacesAndNewlines)
+    var shortestWord = words.first
+    guard var shortestWord = shortestWord else {
+        return 0
+    }
+    for word in words {
+        if word.count < shortestWord.count {
+            shortestWord = word
+        }
+    }
+    return shortestWord.count
+//    return str.components(separatedBy: " ").map { $0.count }.min() ?? 0
+}
